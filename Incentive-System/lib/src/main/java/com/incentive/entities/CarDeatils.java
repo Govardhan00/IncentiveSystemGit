7f79d761432ac034bd9ba2a0e1f008d72a6b744b
Govardhan00 <103577617+Govardhan00@users.noreply.github.com>
2022-05-25 13:38:30 +0530

    cardetails

--- Incentive-System/lib/src/main/java/com/incentive/entities/CarDeatils.java
+++ Incentive-System/lib/src/main/java/com/incentive/entities/CarDeatils.java
@@ -6,38 +6,6 @@ import javax.persistence.Id;
 import javax.persistence.SequenceGenerator;
 
 public class CarDeatils {
-	@Id
-	@SequenceGenerator(name="chassis_seq",initialValue = 12000290,
-								sequenceName = "chassis_seq",allocationSize = 2)
-	@GeneratedValue(generator = "chassis_seq",strategy = GenerationType.SEQUENCE)
-	private int carChassisNumber;
-	private String carModel;
-	private double price;
-	private String specifications;
-	public int getCarChassisNumber() {
-		return carChassisNumber;
-	}
-	public void setCarChassisNumber(int carChassisNumber) {
-		this.carChassisNumber = carChassisNumber;
-	}
-	public String getCarModel() {
-		return carModel;
-	}
-	public void setCarModel(String carModel) {
-		this.carModel = carModel;
-	}
-	public double getPrice() {
-		return price;
-	}
-	public void setPrice(double price) {
-		this.price = price;
-	}
-	public String getSpecifications() {
-		return specifications;
-	}
-	public void setSpecifications(String specifications) {
-		this.specifications = specifications;
-	}
 	
 	
 }
